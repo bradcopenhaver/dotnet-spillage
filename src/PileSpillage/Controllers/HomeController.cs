5fade16b53eece0cc94723df325fd95bbea7bcda
Brad Copenhaver and Erik Killops <brad+erik@>
2017-02-14 14:38:42 -0800

    display quesitons on Home/Index

--- src/PileSpillage/Controllers/HomeController.cs
+++ src/PileSpillage/Controllers/HomeController.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using PileSpillage.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PileSpillage.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Question> allQuestions = _db.Questions.Include(q => q.Answers).Include(q => q.Author).ToList();
+            return View(allQuestions);
         }
     }
 }
